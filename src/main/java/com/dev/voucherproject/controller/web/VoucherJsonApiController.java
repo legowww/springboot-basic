b5648ac366aae94f11e118dc8a73be35f5806604
PHJ <legowww@gmail.com>
2023-07-14 16:58:50 +0900

    style: 컨트롤러에서 VoucherApplication -> VoucherService 이름 변경

--- src/main/java/com/dev/voucherproject/controller/web/VoucherJsonApiController.java
+++ src/main/java/com/dev/voucherproject/controller/web/VoucherJsonApiController.java
@@ -3,7 +3,7 @@ package com.dev.voucherproject.controller.web;
 
 import com.dev.voucherproject.controller.web.request.VoucherCreateRequest;
 import com.dev.voucherproject.controller.web.response.Response;
-import com.dev.voucherproject.model.voucher.VoucherApplication;
+import com.dev.voucherproject.model.service.VoucherService;
 import com.dev.voucherproject.model.voucher.VoucherDto;
 import com.dev.voucherproject.model.voucher.VoucherPolicy;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -18,16 +18,16 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/api/v1/vouchers", produces = MediaType.APPLICATION_JSON_VALUE)
 public class VoucherJsonApiController {
-    private final VoucherApplication voucherApplication;
+    private final VoucherService voucherService;
 
-    public VoucherJsonApiController(VoucherApplication voucherApplication) {
-        this.voucherApplication = voucherApplication;
+    public VoucherJsonApiController(VoucherService voucherService) {
+        this.voucherService = voucherService;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public Response<Void> create(@RequestBody VoucherCreateRequest voucherCreateRequest) {
-        voucherApplication.insert(voucherCreateRequest);
+        voucherService.insert(voucherCreateRequest);
 
         return Response.success(null);
     }
@@ -35,8 +35,8 @@ public class VoucherJsonApiController {
     @GetMapping
     public Response<List<VoucherDto>> vouchers(@RequestParam("policy") Optional<VoucherPolicy> policy) {
         List<VoucherDto> vouchers = policy
-                .map(voucherApplication::findAllVouchersByPolicy)
-                .orElse(voucherApplication.findAllVouchers())
+                .map(voucherService::findAllVouchersByPolicy)
+                .orElse(voucherService.findAllVouchers())
                 .stream().toList();
 
         return Response.success(vouchers);
@@ -47,21 +47,21 @@ public class VoucherJsonApiController {
             @RequestParam("startDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
             @RequestParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
     ) {
-        List<VoucherDto> vouchers = voucherApplication.findAllBetweenDates(startDate, endDate);
+        List<VoucherDto> vouchers = voucherService.findAllBetweenDates(startDate, endDate);
 
         return Response.success(vouchers);
     }
 
     @GetMapping("/{id}")
     public Response<VoucherDto> voucher(@PathVariable String id) {
-        VoucherDto voucher = voucherApplication.findById(id);
+        VoucherDto voucher = voucherService.findById(id);
 
         return Response.success(voucher);
     }
 
     @DeleteMapping("/{id}")
     public Response<Void> delete(@PathVariable String id) {
-        voucherApplication.deleteById(id);
+        voucherService.deleteById(id);
 
         return Response.success(null);
     }
